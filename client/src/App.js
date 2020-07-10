24c9564dd64327d0317bc41a0c716615005b7a1f
Michelle Rodriguez Harrison <dev@michellerh.com>
2020-07-10 19:16:37 -0400

    Debugged

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+// import SavedList from './Movies/SavedList';
+// import { Route } from 'react-router-dom';
+// import MovieList from './Movies/MovieList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
+  // const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
@@ -12,7 +15,8 @@ const App = () => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovieList(response.data);
-          // console.log(movieList);
+          const listOfMovies = response;
+          console.log("Movies", listOfMovies);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -21,15 +25,21 @@ const App = () => {
     getMovies();
   }, []);
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+
+
+  // const addToSavedList = movie => {
+  //   setSavedList([...savedList, movie]);
+  // };
+
+
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <div>
-      </div>
+      {movieList.map(movie => (
+        <Movie key={movie.id} film={movie} />
+      ))}
+      {/* <SavedList list={savedList} /> */}
+      {/* <Route path='/' component={MovieList} /> */}
     </div>
   );
 };
